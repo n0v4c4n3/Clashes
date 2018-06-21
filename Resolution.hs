5e94cee20422b2f02a87f56144333d2df9714784
Gaby RM <info@gabyrm.com>
2018-06-21 18:13:42 -0300

    1st commit

---
+++ Resolution.hs
@@ -0,0 +1,122 @@
+
+module Resolution(sat, tau, valid, V, F(..), Statement, L(..), C, CSet, Clash) where
+
+-----------------------------------------
+-- Tipos de datos
+-----------------------------------------
+type V = String
+data F = Atom V
+       | Neg  F
+       | Conj F F                  
+       | Disy F F     
+       | Imp  F F
+       deriving(Eq)     
+
+type Statement = ([F],F)
+
+data L = LP V | LN V
+         deriving (Eq)
+type C    = [L]
+type CSet = [C]
+type Clash = (C,L,C,L)
+
+-----------------------------------------
+-- Funciones principales
+-----------------------------------------
+-- Pos: retorna True si la formula es SAT, o False si es UNSAT
+sat :: F -> Bool
+sat = undefined
+
+-- Pos: retorna True si la formula es Tautología, o False en caso contrario
+tau :: F -> Bool
+tau = undefined
+
+-- Pos: retorna True si el razonamiento es válido, o False en caso contrario
+valid :: Statement -> Bool
+valid = undefined
+
+-----------------------------------------
+-- Formulas y Clausulas
+-----------------------------------------
+-- Pos: convierte una formula a un conjunto de clausulas
+f2CSet :: F -> CSet
+f2CSet f = cnf2CSet (f2cnf f)
+
+-- Pre: recibe una formula en FNC
+-- Pos: convierte la FNC a un conjunto de clausulas
+cnf2CSet :: F -> CSet
+cnf2CSet (Atom v)     = [[LP v]]
+cnf2CSet (Neg (Atom v)) = [[LN v]]
+cnf2CSet (a `Conj` b) = (cnf2CSet a) ++ (cnf2CSet b)
+cnf2CSet (a `Disy` b) = (aux a) : [(aux b)]
+
+aux :: F -> [L]
+aux (Atom v) = [LP v]
+aux (Neg (Atom v)) = [LN v]
+aux (a `Disy` b) = (aux a) ++ (aux b)
+
+lim :: [L] -> [L]
+lim (x:y:xs) = x : lim (filter (== x) y:xs)
+
+--lim :: [C] -> [C]
+--lim = 
+
+--  ((Atom "p") `Disy` ((Atom "q") `Disy` (Atom "r")))
+-- Pos: convierte una formula a FNC
+f2cnf :: F -> F
+f2cnf = distr . pushNeg . sustImp
+
+sustImp :: F -> F
+sustImp (Atom v)     = (Atom v)
+sustImp (Neg a)      = Neg (sustImp a)
+sustImp (a `Conj` b) = (sustImp a) `Conj` (sustImp b)
+sustImp (a `Disy` b) = (sustImp a) `Disy` (sustImp b)
+sustImp (a `Imp`  b) = (Neg (sustImp a)) `Disy` (sustImp b) 
+
+pushNeg :: F -> F
+pushNeg (Atom v)           = (Atom v)
+pushNeg (Neg (a `Disy` b)) = (pushNeg (Neg a)) `Conj` (pushNeg (Neg b))
+pushNeg (Neg (a `Conj` b)) = (pushNeg (Neg a)) `Disy` (pushNeg (Neg b))
+pushNeg (Neg (Neg a))      = pushNeg a
+pushNeg (Neg a)            = Neg (pushNeg a)
+pushNeg (a `Conj` b)       = (pushNeg a) `Conj` (pushNeg b)
+pushNeg (a `Disy` b)       = (pushNeg a) `Disy` (pushNeg b)
+
+distr :: F -> F
+distr (a `Disy` b) = distr' ((distr a) `Disy` (distr b))
+  where 
+  distr' (a `Disy` (b `Conj` c)) = (distr' (a `Disy` b)) `Conj` (distr' (a `Disy` c))
+  distr' ((a `Conj` b) `Disy` c) = (distr' (a `Disy` c)) `Conj` (distr' (b `Disy` c))
+  distr' a                       = a
+distr (a `Conj` b) = (distr a) `Conj` (distr b)
+distr a            = a  
+
+
+-----------------------------------------
+-- Procedimiento de Resolución
+-----------------------------------------
+-- Pre: recibe un conjunto de clausulas
+-- Pos: si es SAT,   retorna el conjunto de clausulas saturado
+--      si es UNSAT, retorna un conjunto de clausulas incluyendo la clausula vacía
+resolveCSet :: CSet -> CSet
+resolveCSet = undefined
+               
+-- Pos: retorna la resolvente de un conflicto
+resolveClash :: Clash -> C
+resolveClash = undefined
+
+
+----------------------------------------------------------------------------------
+-- Pretty Printing
+instance Show F where
+  show (Atom v)       = v
+  show (Neg (Neg a))  = "~" ++ show (Neg a)
+  show (Neg (Atom v)) = "~ " ++ show (Atom v)
+  show (Neg a)        = "~ (" ++ show a ++ ")"
+  show (a `Conj` b)   = "(" ++ show a ++ ") /\\ (" ++ show b ++ ")"
+  show (a `Disy` b)   = "(" ++ show a ++ ") \\/ (" ++ show b ++ ")"
+  show (a `Imp` b)    = "(" ++ show a ++ ") --> (" ++ show b ++ ")"
+  
+instance Show L where  
+  show (LP v)  = "~" ++ v
+  show (LN v)  = v  
---
+++ Test.hs
@@ -0,0 +1,93 @@
+
+module Test where
+
+import Resolution
+import Test.HUnit  -- Acerca de HUnit: https://hackage.haskell.org/package/HUnit
+
+tau1_sat_should_be_true  = TestCase (assertEqual "sat tau1" True  (sat tau1))
+tau2_sat_should_be_true  = TestCase (assertEqual "sat tau2" True  (sat tau2))
+tau3_sat_should_be_true  = TestCase (assertEqual "sat tau3" True  (sat tau3))
+ind1_sat_should_be_true  = TestCase (assertEqual "sat ind1" True  (sat ind1))
+ind2_sat_should_be_true  = TestCase (assertEqual "sat ind2" True  (sat ind2))
+ind3_sat_should_be_true  = TestCase (assertEqual "sat ind3" True  (sat ind3))
+con1_sat_should_be_false = TestCase (assertEqual "sat con1" False (sat con1))
+con2_sat_should_be_false = TestCase (assertEqual "sat con2" False (sat con2))
+con3_sat_should_be_false = TestCase (assertEqual "sat con3" False (sat con3))
+
+tau1_tau_should_be_true  = TestCase (assertEqual "tau tau1" True  (tau tau1))
+tau2_tau_should_be_true  = TestCase (assertEqual "tau tau2" True  (tau tau2))
+tau3_tau_should_be_true  = TestCase (assertEqual "tau tau3" True  (tau tau3))
+ind1_tau_should_be_false = TestCase (assertEqual "tau ind1" False (tau ind1))
+ind2_tau_should_be_false = TestCase (assertEqual "tau ind2" False (tau ind2))
+ind3_tau_should_be_false = TestCase (assertEqual "tau ind3" False (tau ind3))
+con1_tau_should_be_false = TestCase (assertEqual "tau con1" False (tau con1))
+con2_tau_should_be_false = TestCase (assertEqual "tau con2" False (tau con2))
+con3_tau_should_be_false = TestCase (assertEqual "tau con3" False (tau con3))
+
+val1_val_should_be_true  = TestCase (assertEqual "valid val1" True (valid val1))
+val2_val_should_be_true  = TestCase (assertEqual "valid val2" True (valid val2))
+val3_val_should_be_true  = TestCase (assertEqual "valid val3" True (valid val3))
+inv1_val_should_be_false = TestCase (assertEqual "valid inv1" False (valid inv1))
+inv2_val_should_be_false = TestCase (assertEqual "valid inv2" False (valid inv2))
+inv3_val_should_be_false = TestCase (assertEqual "valid inv3" False (valid inv3))
+
+
+satTests = [TestLabel "Tautologia 1"    tau1_sat_should_be_true, 
+            TestLabel "Tautologia 2"    tau2_sat_should_be_true,
+            TestLabel "Tautologia 3"    tau3_sat_should_be_true,
+            TestLabel "Contingencia 1"  ind1_sat_should_be_true, 
+            TestLabel "Contingencia 2"  ind2_sat_should_be_true,
+            TestLabel "Contingencia 3"  ind3_sat_should_be_true,
+            TestLabel "Contradiccion 1" con1_sat_should_be_false, 
+            TestLabel "Contradiccion 2" con2_sat_should_be_false,
+            TestLabel "Contradiccion 3" con3_sat_should_be_false]
+                     
+tauTests = [TestLabel "Tautologia 1"    tau1_tau_should_be_true, 
+            TestLabel "Tautologia 2"    tau2_tau_should_be_true,
+            TestLabel "Tautologia 3"    tau3_tau_should_be_true,
+            TestLabel "Contingencia 1"  ind1_tau_should_be_false, 
+            TestLabel "Contingencia 2"  ind2_tau_should_be_false,
+            TestLabel "Contingencia 3"  ind3_tau_should_be_false,
+            TestLabel "Contradiccion 1" con1_tau_should_be_false, 
+            TestLabel "Contradiccion 2" con2_tau_should_be_false,
+            TestLabel "Contradiccion 3" con3_tau_should_be_false]                     
+       
+valTests = [TestLabel "Razonamiento valido 1" val1_val_should_be_true, 
+            TestLabel "Razonamiento valido 2" val2_val_should_be_true,
+            TestLabel "Razonamiento valido 3" val3_val_should_be_true,
+            TestLabel "Razonamiento invalido 1" inv1_val_should_be_false, 
+            TestLabel "Razonamiento invalido 2" inv2_val_should_be_false,
+            TestLabel "Razonamiento invalido 3" inv3_val_should_be_false]
+
+allTests = TestList $ satTests ++ tauTests ++ valTests                    
+
+
+-- razonamiento valido    
+val1 = ([], (Atom "q") `Imp` (Atom "q"))
+val2 = ([((Atom "q") `Conj` (Atom "r")) `Imp` (Atom "p"), (Neg (Atom "q")) `Disy` (Atom "r"), Atom "q"],  (Atom "p") `Conj` (Atom "q"))
+val3 = ([(Neg (Atom "p")) `Disy` (Neg (Atom "q")) `Disy` (Atom "r"), Atom "p", Atom "q"],  Atom "r")
+  
+-- razonamiento invalido  
+inv1 = ([ ((Atom "p") `Conj` (Atom "q")) `Imp` (Atom "r"), Atom "p", Atom "q"], Neg (Atom "r") )
+inv2 = ([ (Atom "p") `Imp` ((Atom "q") `Conj` (Atom "r")), Neg (Atom "p")], Neg (Atom "q") `Conj` Neg (Atom "r"))  -- no tiene resolventes
+inv3 = ([ (Neg (Atom "p")) `Disy` ((Atom "q") `Disy` (Atom "r")), (Neg (Atom "p")) `Disy` (Atom "r"), (Atom "p") `Disy` (Neg (Atom "q")) ], Neg (Atom "q") `Conj` Neg (Atom "r"))  
+  
+-- tautologias
+tau1 = true
+tau2 = (Atom "p") `Imp` (Atom "p")
+tau3 = (((Atom "p") `Imp` (Atom "q")) `Imp` (Atom "p")) `Imp` (Atom "p")
+
+-- contingencias
+ind1 = (Atom "p") `Imp` (Atom "q")
+ind2 = ((Neg (Atom "p")) `Imp` (Neg (Atom "q"))) `Imp` ((Atom "p") `Imp` (Atom "q"))
+ind3 = (((Atom "s") `Imp` (Atom "r")) `Disy` ((Atom "q") `Imp` (Neg (Atom "r")))) `Conj` (Atom "p")
+           
+-- contradicciones
+con1 = false
+con2 = true `Imp` false
+con3 = false `Conj` ((Atom "q") `Disy` (Neg (Atom "q")))
+
+
+-- Bottom y Top definidas como abreviaciones sintacticas en el lenguaje L
+false = (Atom "p") `Conj` (Neg (Atom "p"))
+true  = false `Imp` false
